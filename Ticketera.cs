6938bcc66cb9aded86848c9313355308080147e8
JOAQUIN PIVA <47870976@ort>
2023-04-18 11:48:18 -0300

    Ticketera

--- Ticketera.cs
+++ Ticketera.cs
@@ -2,17 +2,17 @@ using System;
 using System.Collections.Generic;
 class Ticketera
 { 
-    private Dictionary<int,Cliente> DicClientes;
+    private static Dictionary<int,Cliente> DicClientes;
 
-    private int UltimoIDEntrada = 1;
+    private static  int UltimoIDEntrada = 1;
 
     
-    int DevolverUltimoID()
+    public static int DevolverUltimoID()
     {  
         return UltimoIDEntrada;
     }
 
-    int AgregarCliente(Cliente cliente)
+    public static int AgregarCliente(Cliente cliente)
     {
         DicClientes.Add(UltimoIDEntrada, cliente);
         UltimoIDEntrada ++;
@@ -20,9 +20,66 @@ class Ticketera
         return UltimoIDEntrada;
     }
 
-    Cliente BuscarCliente(int UltimoIDEntrada)
+    public static Cliente BuscarCliente(int IDEntradaIngresado)
     {
-        foreach()
+        Cliente clienteEncontrado;
+
+        if(DicClientes.ContainsKey(IDEntradaIngresado))
+        {
+            clienteEncontrado = DicClientes[IDEntradaIngresado];
+        }
+        else
+        {
+            clienteEncontrado = null;
+        }
+        return clienteEncontrado;
     }
+    
+    public static bool CambiarEntrada(int IDEntradaIngresado, int tipoEntradaNuevo)
+    {
+        bool puedeCambiar = false;
+        int tipoEntradaVieja;
+        tipoEntradaVieja = DicClientes[IDEntradaIngresado].TipoEntrada;
+        int importeNuevo;
+        int importeViejo;
+
+        importeNuevo = CalcularImporte(tipoEntradaNuevo);
+        importeViejo = CalcularImporte(tipoEntradaVieja);
+
+        if(importeNuevo > importeViejo)
+        {
+            puedeCambiar = true;
+        }
+        return puedeCambiar;
+    }
+
+    public static List<string> EstadisticasTicketera()
+    {
+        
+    }
+
+    public static int CalcularImporte(int tipoEntrada)
+    {
+        int importe;
+        if(tipoEntrada == 1)
+        {
+            importe = 15000;
+        }
+        else if(tipoEntrada == 2)
+        {
+            importe = 30000;
+        }
+        else if(tipoEntrada == 3)
+        {
+            importe = 10000;
+        }
+        else
+        {
+            importe = 40000;
+        }
+        return importe;
+    }
+
+
 
 }
